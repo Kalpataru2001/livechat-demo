de859abfd5d4731cae2cd46abf5ab54d7a5e60db
Kalpataru sahoo <139382993+Kalpataru2001@users.noreply.github.com>
2025-12-11 14:20:11 +0530

    Change log message format for server start

--- server.js
+++ server.js
@@ -88,5 +88,5 @@ io.on('connection', socket => {
 // Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
